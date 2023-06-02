7ef8b3769e6fba61e45044b3388fa38f6e33e5c6
Esmail <e.elturky@elham.sa>
2023-06-03 02:40:53 +0300

    add relations to models

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Post
+from .models import Author, Post, Tag
 
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Author)
 admin.site.register(Post)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,40 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(null=True, max_length=100)
-    photo = models.ImageField(upload_to='profiles_photos/')
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
 
 
 class Post(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='posts_photos/')
-    content = models.TextField()
+    excerpt = models.CharField(max_length=100)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
 
     def __str__(self):
-        return self.title
+        return f"{self.title} on {self.date}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+    posts = models.ManyToManyField(Post, related_name='tags')
+
+    def __str__(self):
+        return self.caption
